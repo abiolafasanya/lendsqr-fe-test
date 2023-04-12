97242a6eca7a7b2a3875e3749210359e0dd80d85
fastbeetech <hezekiahfasanya@gmail.com>
2023-04-12 22:06:05 +0100

    edited info card content under table component

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -6,20 +6,22 @@ import { Link } from 'react-router-dom'
 const InfoCard: React.FC<{id: string}> = ({id}) => {
   return (
     <div className={styles.card}>
+      <Link to={`/user/${id}`}>
         <div className={styles.info}>
-            <img src={eyes} alt="view" />
-            <Link to={`/user/${id}`}>View User</Link>
+          <img src={eyes} alt="view" />
+          <span> View User</span>
         </div>
-        <div className={styles.info}>
-            <img src={blacklisted} alt="blacklist" />
-            <span>Blacklist User</span>
-        </div>
-        <div className={styles.info}>
-            <img src={activateUser} alt="activate" />
-            <span>Activate User</span>
-        </div>
+      </Link>
+      <div className={styles.info}>
+        <img src={blacklisted} alt="blacklist" />
+        <span>Blacklist User</span>
+      </div>
+      <div className={styles.info}>
+        <img src={activateUser} alt="activate" />
+        <span>Activate User</span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default InfoCard
